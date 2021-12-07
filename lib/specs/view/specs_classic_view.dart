d77efd0752d0fb39eabe08643784759909ad4ec5
DESKTOP-IUO5HV7\Frentree <ldg0315@github.com>
2021-12-07 16:10:08 +0900

    거래명세서 수정

--- lib/specs/view/specs_classic_view.dart
+++ lib/specs/view/specs_classic_view.dart
@@ -57,20 +57,20 @@ class SpecsClassicView extends StatelessWidget {
           children: [
             Row(
               children: [
-                const _TextContainer(title: '페이지', width: 100, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.page, width: 135, height: 50, isTitle: false),
+                const _TextContainer(title: '페이지', width: 100, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.page, width: 135, height: 50, isTitle: false, isBold: false,),
               ],
             ),
             Row(
               children: [
-                const _TextContainer(title: '발행일자', width: 100, height: 50, isTitle: true),
-                _TextContainer(title: '${specsModel.date.year}.${specsModel.date.month}.${specsModel.date.day}', width: 135, height: 50, isTitle: false),
+                const _TextContainer(title: '발행일자', width: 100, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: '${specsModel.date.year}.${specsModel.date.month}.${specsModel.date.day}', width: 135, height: 50, isTitle: false, isBold: false,),
               ],
             ),
             Row(
               children: [
-                const _TextContainer(title: '거래처명', width: 100, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.counterpart, width: 135, height: 50, isTitle: false),
+                const _TextContainer(title: '거래처명', width: 100, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.counterpart, width: 135, height: 50, isTitle: false, isBold: false,),
               ],
             ),
             Container(
@@ -97,45 +97,55 @@ class SpecsClassicView extends StatelessWidget {
                 color: innerCellBlueColor,
               ),
             ),
-            _TextContainer(title: specsModel.totalPrice, width: 320, height: 100, isTitle: false),
+            Container(
+              alignment: Alignment.center,
+              child: Text(specsModel.totalPrice, textAlign: TextAlign.center, style: const TextStyle(fontSize: 19.0, fontWeight: FontWeight.bold),),
+              width: 320,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(color: tradeBorderGreyColor),
+                // color: isTitle?innerCellBlueColor:Colors.white,
+    ),
+    )
+            // _TextContainer(title: specsModel.totalPrice, width: 320, height: 100, isTitle: false, isBold: true,),
           ],
         ),
         Column(
           children: [
             Row(
               children: [
-                const _TextContainer(title: '사업자 등록번호', width: 235, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.registrationNum, width: 235, height: 50, isTitle: false),
+                const _TextContainer(title: '사업자 등록번호', width: 235, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.registrationNum, width: 235, height: 50, isTitle: false, isBold: false,),
               ],
             ),
             Row(
               children: [
-                const _TextContainer(title: '상호', width: 80, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.company, width: 155, height: 50, isTitle: false),
-                const _TextContainer(title: '대표자', width: 80, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.providerName, width: 155, height: 50, isTitle: false),
+                const _TextContainer(title: '상호', width: 80, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.company, width: 155, height: 50, isTitle: false, isBold: false,),
+                const _TextContainer(title: '대표자', width: 80, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.providerName, width: 155, height: 50, isTitle: false, isBold: false,),
               ],
             ),
             Row(
               children: [
-                const _TextContainer(title: '주소', width: 80, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.officeAddress, width: 390, height: 50, isTitle: false),
+                const _TextContainer(title: '주소', width: 80, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.officeAddress, width: 390, height: 50, isTitle: false, isBold: false,),
               ],
             ),
             Row(
               children: [
-                const _TextContainer(title: '업태', width: 80, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.industry, width: 155, height: 50, isTitle: false),
-                const _TextContainer(title: '종목', width: 80, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.product, width: 155, height: 50, isTitle: false),
+                const _TextContainer(title: '업태', width: 80, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.industry, width: 155, height: 50, isTitle: false, isBold: false,),
+                const _TextContainer(title: '종목', width: 80, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.product, width: 155, height: 50, isTitle: false, isBold: false,),
               ],
             ),
             Row(
               children: [
-                const _TextContainer(title: '전화', width: 80, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.telephone, width: 155, height: 50, isTitle: false),
-                const _TextContainer(title: '팩스', width: 80, height: 50, isTitle: true),
-                _TextContainer(title: specsModel.fax, width: 155, height: 50, isTitle: false),
+                const _TextContainer(title: '전화', width: 80, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.telephone, width: 155, height: 50, isTitle: false, isBold: false,),
+                const _TextContainer(title: '팩스', width: 80, height: 50, isTitle: true, isBold: false,),
+                _TextContainer(title: specsModel.fax, width: 155, height: 50, isTitle: false, isBold: false,),
               ],
             ),
           ],
@@ -148,14 +158,14 @@ class SpecsClassicView extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: const [
-        _TextContainer(title: '품목코드', width: 130, height: 40, isTitle: true),
-        _TextContainer(title: '품목', width: 185, height: 40, isTitle: true),
-        _TextContainer(title: '상세내용', width: 105, height: 40, isTitle: true),
-        _TextContainer(title: '규격', width: 110, height: 40, isTitle: true),
-        _TextContainer(title: '수량', width: 105, height: 40, isTitle: true),
-        _TextContainer(title: '단가', width: 130, height: 40, isTitle: true),
-        _TextContainer(title: '공급가액', width: 130, height: 40, isTitle: true),
-        _TextContainer(title: '세액', width: 130, height: 40, isTitle: true),
+        _TextContainer(title: '품목코드', width: 130, height: 40, isTitle: true, isBold: false,),
+        _TextContainer(title: '품목', width: 185, height: 40, isTitle: true, isBold: false,),
+        _TextContainer(title: '상세내용', width: 105, height: 40, isTitle: true, isBold: false,),
+        _TextContainer(title: '규격', width: 110, height: 40, isTitle: true, isBold: false,),
+        _TextContainer(title: '수량', width: 105, height: 40, isTitle: true, isBold: false,),
+        _TextContainer(title: '단가', width: 130, height: 40, isTitle: true, isBold: false,),
+        _TextContainer(title: '공급가액', width: 130, height: 40, isTitle: true, isBold: false,),
+        _TextContainer(title: '세액', width: 130, height: 40, isTitle: true, isBold: false,),
       ],
     );
   }
@@ -164,14 +174,14 @@ class SpecsClassicView extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        _TextContainer(title: items[i].productCode, width: 130, height: 40, isTitle: false),
-        _TextContainer(title: items[i].productName, width: 185, height: 40, isTitle: false),
-        _TextContainer(title: items[i].detailInfo, width: 105, height: 40, isTitle: false),
-        _TextContainer(title: items[i].size, width: 110, height: 40, isTitle: false),
-        _TextContainer(title: items[i].amount.toString(), width: 105, height: 40, isTitle: false),
-        _TextContainer(title: items[i].unitPrice.toString(), width: 130, height: 40, isTitle: false),
-        _TextContainer(title: items[i].valueOfSupply.toString(), width: 130, height: 40, isTitle: false),
-        _TextContainer(title: items[i].tax.toString(), width: 130, height: 40, isTitle: false),
+        _TextContainer(title: items[i].productCode, width: 130, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: items[i].productName, width: 185, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: items[i].detailInfo, width: 105, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: items[i].size, width: 110, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: items[i].amount.toString(), width: 105, height: 40, isTitle: false, isBold: false,),
+        _NumContainer(title: items[i].unitPrice.toString(), width: 130, height: 40, isTitle: false, isBold: false,),
+        _NumContainer(title: items[i].valueOfSupply.toString(), width: 130, height: 40, isTitle: false, isBold: false,),
+        _NumContainer(title: items[i].tax.toString(), width: 130, height: 40, isTitle: false, isBold: false,),
       ],
     );
   }
@@ -180,14 +190,14 @@ class SpecsClassicView extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: const [
-        _TextContainer(title: '', width: 130, height: 40, isTitle: false),
-        _TextContainer(title: '', width: 185, height: 40, isTitle: false),
-        _TextContainer(title: '', width: 105, height: 40, isTitle: false),
-        _TextContainer(title: '', width: 110, height: 40, isTitle: false),
-        _TextContainer(title: '', width: 105, height: 40, isTitle: false),
-        _TextContainer(title: '', width: 130, height: 40, isTitle: false),
-        _TextContainer(title: '', width: 130, height: 40, isTitle: false),
-        _TextContainer(title: '', width: 130, height: 40, isTitle: false),
+        _TextContainer(title: '', width: 130, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: '', width: 185, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: '', width: 105, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: '', width: 110, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: '', width: 105, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: '', width: 130, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: '', width: 130, height: 40, isTitle: false, isBold: false,),
+        _TextContainer(title: '', width: 130, height: 40, isTitle: false, isBold: false,),
       ],
     );
   }
@@ -206,9 +216,9 @@ class SpecsClassicView extends StatelessWidget {
               color: Colors.white
           ),
         ),
-        const _TextContainer(title: '합계', width: 130, height: 50, isTitle: true),
-        _TextContainer(title: specsModel.total.toString(), width: 130, height: 50, isTitle: false),
-        _TextContainer(title: (specsModel.total/10).toString(), width: 130, height: 50, isTitle: false),
+        const _TextContainer(title: '합계', width: 130, height: 50, isTitle: true, isBold: false,),
+        _NumContainer(title: specsModel.total.toString(), width: 130, height: 50, isTitle: false, isBold: false,),
+        _NumContainer(title: (specsModel.total/10).toString(), width: 130, height: 50, isTitle: false, isBold: false,),
       ],
     );
   }
@@ -217,11 +227,11 @@ class SpecsClassicView extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const _TextContainer(title: '전잔금', width: 420, height: 50, isTitle: true),
-        _TextContainer(title: specsModel.exBalance.toString(), width: 215, height: 50, isTitle: false),
-        const _TextContainer(title: '', width: 130, height: 50, isTitle: false),
-        const _TextContainer(title: '총금액', width: 130, height: 50, isTitle: true),
-        _TextContainer(title: specsModel.totalPrice2.toString(), width: 130, height: 50, isTitle: false),
+        const _TextContainer(title: '전잔금', width: 420, height: 50, isTitle: true, isBold: true,),
+        _NumContainer(title: specsModel.exBalance.toString(), width: 215, height: 50, isTitle: false, isBold: true,),
+        const _TextContainer(title: '', width: 130, height: 50, isTitle: false, isBold: false,),
+        const _TextContainer(title: '총금액', width: 130, height: 50, isTitle: true, isBold: true,),
+        _NumContainer(title: specsModel.totalPrice2.toString(), width: 130, height: 50, isTitle: false, isBold: true,),
       ],
     );
   }
@@ -230,13 +240,13 @@ class SpecsClassicView extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const _TextContainer(title: '입금', width: 130, height: 50, isTitle: true),
-        const _TextContainer(title: '', width: 185, height: 50, isTitle: false),
-        const _TextContainer(title: '잔금', width: 105, height: 50, isTitle: true),
-        _TextContainer(title: specsModel.balance.toString(), width: 215, height: 50, isTitle: false),
-        const _TextContainer(title: '', width: 130, height: 50, isTitle: false),
-        const _TextContainer(title: '인수자', width: 130, height: 50, isTitle: true),
-        _TextContainer(title: specsModel.underwriter, width: 130, height: 50, isTitle: false),
+        const _TextContainer(title: '입금', width: 130, height: 50, isTitle: true, isBold: true,),
+        const _TextContainer(title: '', width: 185, height: 50, isTitle: false, isBold: true,),
+        const _TextContainer(title: '잔금', width: 105, height: 50, isTitle: true, isBold: true,),
+        _NumContainer(title: specsModel.balance.toString(), width: 215, height: 50, isTitle: false, isBold: true,),
+        const _TextContainer(title: '', width: 130, height: 50, isTitle: false, isBold: false,),
+        const _TextContainer(title: '인수자', width: 130, height: 50, isTitle: true, isBold: true,),
+        _TextContainer(title: specsModel.underwriter, width: 130, height: 50, isTitle: false, isBold: true,),
       ],
     );
   }
@@ -248,13 +258,38 @@ class _TextContainer extends StatelessWidget {
   final double width;
   final double height;
   final bool isTitle;
-  const _TextContainer({Key? key, required this.title, required this.width, required this.height, required this.isTitle}) : super(key: key);
+  final bool isBold;
+  const _TextContainer({Key? key, required this.title, required this.width, required this.height, required this.isTitle, required this.isBold}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: Text(title, textAlign: TextAlign.center,),
+      child: Text(title, textAlign: TextAlign.center, style: TextStyle(fontWeight: isBold?FontWeight.bold:null),),
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        border: Border.all(color: tradeBorderGreyColor),
+        color: isTitle?innerCellBlueColor:Colors.white,
+      ),
+    );
+  }
+}
+
+class _NumContainer extends StatelessWidget {
+  final String title;
+  final double width;
+  final double height;
+  final bool isTitle;
+  final bool isBold;
+  const _NumContainer({Key? key, required this.title, required this.width, required this.height, required this.isTitle, required this.isBold}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      alignment: Alignment.centerRight,
+      child: Text(title, textAlign: TextAlign.center, style: TextStyle(fontWeight: isBold?FontWeight.bold:null),),
       width: width,
       height: height,
       decoration: BoxDecoration(
