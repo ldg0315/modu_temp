46280cdfde22624b3a67fad60ff1553ee91b3b34
DESKTOP-IUO5HV7\Frentree <ldg0315@github.com>
2021-12-09 16:23:23 +0900

    파이어베이스 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:modu_temp/common/provider/specs_mode_provider.dart';
@@ -6,7 +7,9 @@ import 'package:modu_temp/specs/view/specs_popup_view.dart';
 import 'package:provider/provider.dart';
 import 'login/view/login_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   initializeDateFormatting().then((value) => runApp(MyApp()));
 }
 
