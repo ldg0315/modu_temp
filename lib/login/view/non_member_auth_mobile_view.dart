a1c77527e6a61c6ac5b36d2291174b1a2d830526
DESKTOP-IUO5HV7\Frentree <ldg0315@github.com>
2021-12-09 10:28:26 +0900

    비회원 인증 모바일 페이지 추가

---
+++ lib/login/view/non_member_auth_mobile_view.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:modu_temp/common/component/input_column.dart';
+import 'package:modu_temp/constant/color.dart';
+
+class NonMemberAuthMobileView extends StatelessWidget {
+  const NonMemberAuthMobileView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      body: Stack(
+        children: [
+          SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Image.asset('assets/images/back.png', fit: BoxFit.cover,)
+          ),
+          Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50)
+            ),
+            child: SizedBox(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 400,
+                    width: 400,
+                    child: ClipRRect(
+                      child: Image.asset('assets/images/drawing.png', fit: BoxFit.cover),
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                    ),
+                  ),
+                  Container(
+                    height: 400,
+                    width: 400,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                        color: Colors.white
+                    ),
+                    child: Column(
+                      children: [
+                        const Center(
+                          child: Image(
+                            image: AssetImage('assets/images/modu.PNG'),
+                          ),
+                        ),
+                        const SizedBox(height: 20,),
+                        const Text("- 비회원 인증 페이지 -", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0, color: btnNavyColor),),
+                        const SizedBox(height: 20,),
+                        const Text("거래처에서 전송한 거래명세서가 있습니다.\n            아래항목을 입력해 주세요.", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0, color: textOrangeColor),),
+                        const SizedBox(height: 35.0),
+                        const InputColumn(title: "성명", hint: '이름을 입력하세요.', fillColor: inputWhiteColor, borderColor: inputBorderGreyColor),
+                        ButtonTheme(
+                            child: ElevatedButton(
+                              onPressed: (){},
+                              child: const Text("확인"),
+                              style: ElevatedButton.styleFrom(
+                                  fixedSize: const Size(400, 40),
+                                  primary: primaryBlueColor,
+                                  elevation: 0.0
+                              ),
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
